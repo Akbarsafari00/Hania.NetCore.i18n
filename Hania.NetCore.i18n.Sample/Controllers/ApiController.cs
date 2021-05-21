f12db0548719a0e628393d570e67f03791be467d
Akbar Ahmadi Saray <akbarahmadi@utechia.com>
2021-05-21 19:32:26 +0430

    init

---
+++ Hania.NetCore.i18n.Sample/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Hania.NetCore.i18n.Abstracts;
+using Hania.NetCore.i18n.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hania.NetCore.i18n.Sample.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class ApiController : ControllerBase
+    {
+        private readonly IHaniaLocalizer _localizer;
+
+        public ApiController(IHaniaLocalizer localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public Task<string> Get()
+        {
+            return _localizer.Localize(HttpContext.GetAcceptedLanguage(),"User","hi");
+        }
+    }
+}
